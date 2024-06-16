c54444dc2039c2523f044054008e244acbf2bb2b
tzzjp <tzz.zhao@gmail.com>
2024-06-17 00:19:21 +0900

    sitemap追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tzz.dev',
   integrations: [
-    react(), 
+    react(),
     tailwind({
-      applyBaseStyles: false,
-    })],
-    i18n: {
-      defaultLocale: "ja",
-      locales: ["ja"]
-    },
+      applyBaseStyles: false
+    }),
+    sitemap()],
+  i18n: {
+    defaultLocale: "ja",
+    locales: ["ja"]
+  }
 });

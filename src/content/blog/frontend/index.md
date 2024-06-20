84fe335fedf9f33455f161c4cf90cbe39ab269ef
tzzjp <tzz.zhao@gmail.com>
2024-06-20 19:47:34 +0900

    ブログリスト追加

---
+++ src/content/blog/frontend/index.md
@@ -0,0 +1,6 @@
+---
+title: index
+pubDate: 2024-04-04
+---
+
+FrontEnd
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,11 @@ import { rssSchema } from "@astrojs/rss";
 
 const blog = defineCollection({
   type: 'content',
-  schema: rssSchema,
-  // schema: z.object({
-    // title: z.string(),
-  // })
+  // schema: rssSchema,
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+  })
 });
 
 export const collections = {blog};
